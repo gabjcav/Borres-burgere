85a15daea3f925256c11eb46a8288e5114a32003
gabjcav <69957148+gabjcav@users.noreply.github.com>
2021-02-22 17:51:58 +0100

    minor updates

---
+++ components/Container/index.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Container = styled.main`
+    width: 970px;
+    margin: 0 auto; 
+    background-color: whitesmoke; 
+    padding: 10px; 
+`
+
+export default Container; 
