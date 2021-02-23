fb10de42c799ac86819547ba55b1a150504b29b6
gabjcav <69957148+gabjcav@users.noreply.github.com>
2021-02-23 15:56:42 +0100

    adjusted navbar ++

---
+++ pages/drinks.jsx
@@ -0,0 +1,19 @@
+import NavBar from '../components/NavBar';
+import NavContainer from '../components/NavBar';
+import MainContainer from '../components/MainContainer';
+
+const Drinks = () => {
+    return(
+        <>
+            <NavContainer>
+                <NavBar />
+            </NavContainer>
+            <MainContainer>
+                <h1>Drinks</h1>
+            </MainContainer>
+            
+        </>
+    )
+}
+
+export default Drinks;
